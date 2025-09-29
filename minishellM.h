3303edbfc214cc9838c8e4bb497f809699f4170f
Martim Cardoso <mcardoso@c2r2s2.42lisboa.com>
2025-09-29 18:01:43 +0100

    small changes to header

---
+++ minishellM.h
@@ -0,0 +1,16 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <dirent.h>
+
+#endif
