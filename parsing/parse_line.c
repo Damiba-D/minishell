33278fd023c8bc4a2f20d0dccaaa84378e1bb5c6
Martim Cardoso <mcardoso@c1r10s2.42lisboa.com>
2025-10-08 18:39:15 +0100

    started dividing the pipes

--- parsing/parse_line.c
+++ parsing/parse_line.c
@@ -1,6 +1,26 @@
 #include "minishellM.h"
 
-t_input	*parse_input(char *line)
+static int	count_pipes(char *line)
+{
+	int	i;
+	int	count;
+	int	in_quote;
+
+	i = 0;
+	count = 0;
+	in_quote = 0;
+	while (line[i])
+	{
+		if (line[i] == '\'' && (i == 0 || line[i - 1] != '\\'))
+			in_quote = !in_quote;
+		if (line[i] == '|' && !in_quote)
+			count++;
+		i++;
+	}
+	return(count);
+}
+
+t_input	*parse_line(char *line)
 {
 	
 }
