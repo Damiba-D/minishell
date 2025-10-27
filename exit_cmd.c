cce710e02ae3380c1fea7aaf6d806ce14e24fd96
Daniel-Damiba <danielddamiba@gmail.com>
2025-10-27 22:25:30 +0000

    small exit fix

--- exit_cmd.c
+++ exit_cmd.c
@@ -16,14 +16,42 @@ static bool	is_numeric(const char *s)
 		return (false);
 	if (s[i] == '+' || s[i] == '-')
 		i++;
-	if (!isdigit(s[i]))
+	if (!ft_isdigit(s[i]))
 		return (false);
 	while (s[i])
 	{
-		if (!isdigit(s[i]))
+		if (!ft_isdigit(s[i]))
 			return (false);
 		i++;
 	}
+	return (true);
+}
+
+bool	exit_parser(const char *str, long long *out)
+{
+	int			sign;
+	int			i;
+	int			digit;
+
+	if (!is_numeric(str))
+		return (false);
+	i = 0;
+	sign = 1;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i++] == '-')
+			sign = -1;
+	}
+	while (str[i])
+	{
+		digit = str[i++] - '0';
+		if (sign == 1 && (*out > (LLONG_MAX - digit) / 10))
+			return (false);
+		if (sign == -1 && (-(*out) < (LLONG_MIN + digit) / 10))
+			return (false);
+		*out = *out * 10 + digit;
+	}
+	*out *= sign;
 	return (true);
 }
 
@@ -34,7 +62,8 @@ int		exit_cmd(char **args, t_env *env_list)
 	ft_putendl_fd("exit", 2);
 	if (!args[1])
 		exit_cmd_cleanup(args, env_list, 0);
-	if (!is_numeric(args[1]))
+	exit_value = 0;
+	if (!exit_parser(args[1], &exit_value))
 	{
 		ft_putstr_fd("minishell: exit: ", 2);
 		ft_putstr_fd(args[1], 2);
@@ -47,7 +76,6 @@ int		exit_cmd(char **args, t_env *env_list)
 		ft_putendl_fd("minishell: exit: too many arguments", 2);
 		return (1);
 	}
-	exit_value = ft_atoll(args[1]);
 	exit_cmd_cleanup(args, env_list, (unsigned char)exit_value);
 	return (0);
 }
