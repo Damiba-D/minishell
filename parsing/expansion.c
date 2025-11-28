ac427c7fdf8191b6df40bdca91d088620ee5f771
Martim Cardoso <mcardoso@c1r7s3.42lisboa.com>
2025-11-28 19:36:00 +0000

    made extraction func for variable name after $ and a func that takes a var name and returns its value from env

--- parsing/expansion.c
+++ parsing/expansion.c
@@ -1,15 +1,44 @@
-#include "minishell.h"
+#include "../minishell.h"
 
-void	expand_all(t_msh *msh)
+static int	is_var_char(char c)
 {
-	
+	return(ft_isalnum(c) || c == '_');
 }
 
-void	expand_args(t_input *node)
+static void	*extract_var_name(char *str, int *i)
 {
-	int		i;
-	char	*new;
+	int		start;
+	int		len;
+	char	*var_name;
 
-	i = 0;
-	
+	start = *i;
+	len = 0;
+	if (str[*i] == '?')
+	{
+		(*i)++;
+		return (ft_strdup("?"));
+	}
+	while (str[*i] && is_var_char(str[*i]))
+	{
+		(*i)++;
+		len++;
+	}
+	if (len == 0)
+		return (NULL);
+	var_name = ft_substr(str, start, len);
+	return (var_name);
+}
+
+static char	*exp_single_var(char *var_name, t_env *env_list, int lst_exit_stat)
+{
+	char	*value;
+
+	if (!var_name)
+		return(ft_strdup("?"));
+	if (ft_strncmp(var_name, "?", 2) == 0)
+		return (ft_itoa(lst_exit_stat));
+	value = get_env_value(var_name, env_list);
+	if (value)
+		return (ft_strdup(value));
+	return (ft_strdup(""));
 }
