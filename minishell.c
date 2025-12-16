9b24ea5769951453d49469031d061114448ff484
martim <martim.cardoso55@gmail.com>
2025-12-16 16:10:10 +0000

    just for update

--- minishell.c
+++ minishell.c
@@ -1,66 +1,5 @@
 #include "minishell.h"
 
-void	debug_print_input_list(t_list *input_list)
-{
-	t_list	*current;
-	int		i;
-	int 	cmd_num = 0;
-	t_input *input;
-
-	current = input_list;
-	printf("=== DEBUG: Parsed Commands ===\n");
-	while (current)
-	{
-		input = (t_input *)current->content;
-		printf("Command %d:\n", cmd_num++);
-		if (input->argv)
-		{
-			i = 0;
-			while (input->argv[i])
-			{
-				printf("  argv[%d]: %s\n", i, input->argv[i]);
-				i++;
-			}
-		}
-		else
-			printf("  argv: NULL\n");
-		if (input->infiles)
-		{
-			i = 0;
-			while (input->infiles[i].filename)
-			{
-				printf("  infile[%d]: %s\n", i, input->infiles[i].filename);
-				if (input->infiles[i].mode == REDIN)
-					printf("  mode[%d]: infile\n", i);
-				else if (input->infiles[i].mode == HDOC)
-					printf("  mode[%d]: heredoc\n", i);
-				i++;
-			}
-		}
-		else
-			printf("  infile: NULL\n");
-		if (input->outfiles)
-		{
-			i = 0;
-			while (input->outfiles[i].filename)
-			{
-				printf("  outfile[%d]: %s\n", i, input->outfiles[i].filename);
-				if (input->outfiles[i].mode == REDOUT)
-					printf("  mode[%d]: truncate\n", i);
-				else if (input->outfiles[i].mode == APPEND)
-					printf("  mode[%d]: append\n", i);
-				i++;
-			}
-		}
-		else
-			printf("  outfile: NULL\n");
-		printf("  next: %s\n", current->next ? "exists" : "NULL");
-		printf("---\n");
-		current = current->next;
-	}
-	printf("=== END DEBUG ===\n");
-}
-
 int	main(void)
 {
 	init_env(&msh()->env);
@@ -95,7 +34,6 @@ int	main(void)
 			cur = cur->next;
 		}
 		setup_execution_signals();
-		debug_print_input_list(msh()->inputlst);
 		executor();
 		ft_lstclear(&msh()->inputlst, free_input_node);
 		free(msh()->cmdline);
