2ccf735bc26dcc3fad9f785e1c60e5700f2e6b63
Daniel Gabriel Kirnoze Damiba <danielddamiba@gmail.com>
2025-12-16 00:03:24 +0000

    organization of execute_pipeline, executor in general

--- execution/executor/execute_pipeline.c
+++ execution/executor/execute_pipeline.c
@@ -1,13 +1,13 @@
 #include "../../minishell.h"
 #include "executor.h"
 
-void cleanup_failed_pipeline(int prev_read)
+void cleanup_failed_pipeline(char *error, int prev_read)
 {
 	int i;
 	int w_status;
 
 	i = 0;
-	print_err(NULL, "fork", true);
+	print_err(NULL, error, true);
 	while (msh()->pids[i] > 0)
 	{
 		waitpid(msh()->pids[i], &w_status, 0);
@@ -27,8 +27,8 @@ void pipeline_process(t_list *current)
 	char **env;
 
 	node = (t_input *)current->content;
-	
-	setup_fds(node, msh()->og_fds, false);
+	if (setup_fds(node, msh()->og_fds, false))
+		error_exit(NULL, NULL, 1, false);
 	if (is_builtin(node->argv[0]))
 		error_exit(NULL, NULL, run_builtin(node), false);
 	else
@@ -46,6 +46,38 @@ void pipeline_process(t_list *current)
 	}
 }
 
+static void setup_child(int cmd_no, int input_size)
+{
+	save_og_fds(msh()->og_fds);
+	if (msh()->prev_read != -1)
+	{
+		dup2(msh()->prev_read, STDIN_FILENO);
+		close(msh()->prev_read);
+	}
+	if (cmd_no < input_size - 1)
+	{
+		close(msh()->pipe[0]);
+		dup2(msh()->pipe[1], STDOUT_FILENO);
+		close(msh()->pipe[1]);
+	}
+}
+
+static void execute_and_reset(t_list *current, int cmd_no, int input_size)
+{
+	if (msh()->pids[cmd_no] == 0)
+	{
+		setup_child(cmd_no, input_size);
+		pipeline_process(current);
+	}
+	if (msh()->prev_read != -1)
+		close(msh()->prev_read);
+	if (cmd_no < input_size - 1)
+	{
+		close(msh()->pipe[1]);
+		msh()->prev_read = msh()->pipe[0];
+	}
+}
+
 void execute_pipeline(t_list *inputlst, int input_size)
 {
 	int i;
@@ -59,47 +91,15 @@ void execute_pipeline(t_list *inputlst, int input_size)
 	while (current)
 	{
 		if (i < input_size - 1 && pipe(msh()->pipe) == -1)
-		{
-			if (msh()->prev_read != -1)
-				close(msh()->prev_read);
-			cleanup_failed_pipeline(msh()->prev_read);
-		}
+			return (cleanup_failed_pipeline("pipe", msh()->prev_read));
 		msh()->pids[i] = fork();
 		if (msh()->pids[i] == -1)
 		{
-			if (i < input_size)
-			{
-				close(msh()->pipe[0]);
-				close(msh()->pipe[1]);
-			}
-			cleanup_failed_pipeline(msh()->prev_read);
-			return ;
-		}
-		if (msh()->pids[i] == 0)
-		{
-			save_og_fds(msh()->og_fds);
-			if (msh()->prev_read != -1)
-			{
-				dup2(msh()->prev_read, STDIN_FILENO);
-				close(msh()->prev_read);
-			}
 			if (i < input_size - 1)
-			{
-				close(msh()->pipe[0]);  // Close read end
-				dup2(msh()->pipe[1], STDOUT_FILENO);
-				close(msh()->pipe[1]);
-			}
-			pipeline_process(current);
+				close_pipe();
+			return (cleanup_failed_pipeline("fork", msh()->prev_read));
 		}
-		if (msh()->prev_read != -1)
-			close(msh()->prev_read);
-		
-		// Close current pipe write end and save read end
-		if (i < input_size - 1)
-		{
-			close(msh()->pipe[1]);
-			msh()->prev_read = msh()->pipe[0];  // Save for next iteration
-		}
+		execute_and_reset(current, i, input_size);
 		current = current->next;
 		i++;
 	}
